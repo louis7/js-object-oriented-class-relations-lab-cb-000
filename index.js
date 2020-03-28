4a2b89c90f344ee8354883b234bca603f9c84792
louis baptiste <louisvis@ymail.com>
2020-03-28 06:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 let store = {drivers:[], passengers: [], trips:[]}
 let driverId = 0
 let passengerId= 0
+let tripId = 0
 class Driver {
   constructor (name){
     this.name = name
     this.id = driverId++
     store.drivers.push(this)
 
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
   }
 
 
@@ -25,12 +37,20 @@ class Passenger {
 }
 
 class Trip {
-  constructor (driver, passenger){
-    this.driverid = driver.id
-    this.passengerid = passenger.id
-    store.trip.push(this)
-
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
   }
-
-
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
