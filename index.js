ed73b7f842a3f52ecd47d8de541f039e84e35543
louis baptiste <louisvis@ymail.com>
2020-03-28 06:46:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+
+let store = {drivers:[], passengers: [], trips:[]}
+let driverId = 0
+let passengerId= 0
+class Driver {
+  constructor (name){
+    this.name = name
+    this.id = driverId++
+    store.drivers.push(this)
+
+  }
+
+
+}
+
+class Passenger {
+  constructor (name){
+    this.name = name
+    this.id = passengerId++
+    store.passengers.push(this)
+
+  }
+
+
+}
+
+class Trip {
+  constructor (driver, passenger){
+    
+    store.trip.push(this)
+
+  }
+
+
+}
